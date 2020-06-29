ab66707d61d433fe0f7436c80574d949a96e64a5
Lucas De Bonet <35583218+ldebonet@users.noreply.github.com>
2020-06-29 13:39:55 -0400

    Initial commit

---
+++ Main.js
@@ -0,0 +1,20 @@
+const express     = require('express');
+const app         = express();
+const path        = require('path');
+const mysql       = require('mysql');
+const session     = require('express-session');
+const MySQLStore  = require('express-mysql-session')(session);
+const Router      = require('./Router');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+
+console.log('Testing Server');
+
+//database
+const db = mysql.createConnection({
+  host: 'localhost',
+user: 'root',
+  password: 'root',
+  database: 'myapp'
+});
